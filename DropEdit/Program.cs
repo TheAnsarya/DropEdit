6cf7c9a3cf8ef32d28368e1d332910dabc1bae76
Andy Hubbard <me@ansarya.com>
2025-07-18 00:51:33 -0400

    Basic: editorconfig, move curly braces, and change spaces to tabs

--- DropEdit/Program.cs
+++ DropEdit/Program.cs
@@ -1,19 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace DropEdit
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace DropEdit;
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+public class Program {
+	public static async Task Main(string[] args) {
+		var builder = WebAssemblyHostBuilder.CreateDefault(args);
+		builder.RootComponents.Add<App>("#app");
+		builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            await builder.Build().RunAsync();
-        }
-    }
+		builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+		await builder.Build().RunAsync();
+	}
 }
